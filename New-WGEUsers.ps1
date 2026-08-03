a436def348f3bae5272eda4c57859069fd688c75
wael-mozy <waelthis@gmail.com>
2026-08-03 06:47:12 +0300

    Add script to create AD users from CSV file

--- New-WGEUsers.ps1
+++ New-WGEUsers.ps1
@@ -1,1 +1,9 @@
 
+Import-Csv .\employees.csv | ForEach-Object {
+    New-ADUser `
+        -Name "$($_.FirstName) $($_.LastName)" `
+        -GivenName $_.FirstName `
+        -Surname $_.LastName `
+        -Department $_.Department `
+        -Enabled $true
+}
